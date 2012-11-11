50250d6604e07930c9094159a358cf2eeaee0781
shtirlic <serg@podtynnyi.com>
2012-11-11 23:42:19 +0400

    Add some info into basic report

--- lib/project-health/report/basic.rb
+++ lib/project-health/report/basic.rb
@@ -1,14 +1,31 @@
 module ProjectHealth
   class BasicReport
 
-    attr_reader :name
+    attr_reader :name, :title, :language, :stars,
+                :created, :last_push, :description
 
     def initialize(oct, name)
-      @name = name
+      @name        = name
+      repo         = oct.repo(name)
+      @language    = repo["language"]
+      @stars       = repo["watchers_count"]
+      @created     = repo["created_at"]
+      @last_push   = repo["pushed_at"]
+      @description = repo["description"]
     end
 
     def stats
-      { "Name" => name }
+      {
+        "Basic" =>
+        {
+          "Name" => name,
+          "Description" => description,
+          "Created" => created,
+          "Last push" => last_push,
+          "Language" => language,
+          "Stars" => stars
+        }
+       }
     end
   end
 end
