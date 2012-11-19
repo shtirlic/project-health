b8cfc19b960ab5b455b8cc663bddf31cb5b13f6b
shtirlic <serg@podtynnyi.com>
2012-11-19 23:36:40 +0400

    Refactor pull requests report

--- lib/project-health/report/pull_requests.rb
+++ lib/project-health/report/pull_requests.rb
@@ -1,64 +1,64 @@
 module ProjectHealth
   class PullRequestsReport
 
-    attr_reader :pulls_open, :pulls_closed, :pulls_all
+    attr_reader :open, :closed, :all
 
     def initialize(oct, name)
-      @pull_open_time = []
-      @pulls_open   = oct.pull_requests(name, :open).tap{|x| x.each(&method(:calc_open_pull_time)) }.count
-      @pulls_closed = oct.pull_requests(name, :closed).count
-      @pulls_all    = pulls_open + pulls_closed
+      @open_times = []
+      @open   = oct.pull_requests(name, :open).tap{|x| x.each(&method(:calc_open_pull_time)) }.count
+      @closed = oct.pull_requests(name, :closed).count
+      @all    = open + closed
     end
 
     def stats
       {
         "Pull Requests"=>
         {
-          'All'      => pulls_all,
-          'Open'     => pulls_open,
-          'Closed'   => pulls_closed,
-          'Open %'   => pulls_open_percent.to_f.round(2),
-          'Closed %' => pulls_closed_percent.to_f.round(2),
-          'Open/Closed % ratio' => open_closed_pull_ratio.to_f.round(2),
-          'Open time in days' => pull_open_time,
-          'Min open time in days' => pull_min_open_time,
-          'Max open time in days' => pull_max_open_time,
-          'Average days request is opened' => pull_average_open_time,
-          'Health' => pulls_health
+          'All'      => all,
+          'Open'     => open,
+          'Closed'   => closed,
+          'Open %'   => open_percent.to_f.round(2),
+          'Closed %' => closed_percent.to_f.round(2),
+          'Open/Closed % ratio' => open_closed_ratio.to_f.round(2),
+          'Open time in days' => open_time,
+          'Min open time in days' => min_open_time,
+          'Max open time in days' => max_open_time,
+          'Average days request is opened' => average_open_time,
+          'Health' => health
         }
       }.delete_if{|k,v| v.kind_of?(Float) && v.nan?}
     end
 
-    def pulls_open_percent
-      pulls_open.percent_of(pulls_all)
+    def open_percent
+      open.percent_of all
     end
 
-    def pulls_closed_percent
-      pulls_closed.percent_of(pulls_all)
+    def closed_percent
+      closed.percent_of all
     end
 
-    def open_closed_pull_ratio
-      result = pulls_open_percent / pulls_closed_percent
+    def open_closed_ratio
+      result = open_percent / closed_percent
     end
 
-    def pull_open_time
-      @pull_open_time.inject(:+)
+    def open_time
+      @open_times.inject(:+)
     end
 
-    def pull_average_open_time
-      pull_open_time / pulls_open
+    def average_open_time
+      open_time / open
     end
 
-    def pull_min_open_time
-      @pull_open_time.min
+    def min_open_time
+      @open_times.min
     end
 
-    def pull_max_open_time
-      @pull_open_time.max
+    def max_open_time
+      @open_times.max
     end
 
-    def pulls_health
-      case open_closed_pull_ratio.to_f
+    def health
+      case open_closed_ratio.to_f
       when 0..0.1
         "Good"
       when 0.1..0.2
@@ -72,7 +72,7 @@ module ProjectHealth
 
     private
     def calc_open_pull_time(pull)
-      @pull_open_time << (DateTime.now.to_i - pull.created_at.to_datetime.to_i)/60/60/24
+      @open_times << (DateTime.now.to_i - pull.created_at.to_datetime.to_i)/60/60/24
     end
   end
 end
